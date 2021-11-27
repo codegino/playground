46b6f16c4b91c3521e27ff905a6de3db92f90763
codegino <carloginocatapang@gmail.com>
2021-11-27 11:02:26 +0800

    Added edit action

--- app/routes/words/edit.$id.tsx
+++ app/routes/words/edit.$id.tsx
@@ -1,8 +1,23 @@
-import { useLoaderData } from "remix";
-import type { LoaderFunction } from "remix";
+import { useLoaderData, redirect } from "remix";
+import type { LoaderFunction, ActionFunction } from "remix";
 import WordForm from "~/components/WordForm";
 import { Word } from "~/models/word";
 import { supabase } from "~/libs/supabase-client";
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const formData = await request.formData();
+  const id = params.id as string;
+
+  const updates = {
+    type: formData.get("type"),
+    sentences: formData.getAll("sentence"),
+    definitions: formData.getAll("definition"),
+  };
+
+  await supabase.from("words").update(updates).eq("id", id);
+
+  return redirect(`/words/${id}`);
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { data } = await supabase
