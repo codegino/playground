3475200532dc683f656bf6ed3142beb8e0e4ec4e
codegino <carloginocatapang@gmail.com>
2021-12-09 17:38:52 +0800

    Fetch one and delete one

--- app/routes/words/$id.tsx
+++ app/routes/words/$id.tsx
@@ -1,10 +1,17 @@
 import { Form, useLoaderData, redirect, useTransition } from "remix";
 import type { LoaderFunction, ActionFunction } from "remix";
-import { supabase } from "~/libs/supabase-client";
-import { Word } from "~/models/word";
+import type { Word } from "~/models/word";
+import { Input } from "~/components/basic/input";
+import { Button } from "~/components/basic/button";
+import { setAuthToken, supabase } from "~/utils/supabase.server";
+import { useSupabase } from "~/utils/supabase-client";
 
+// Here's the action to handle deletion
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData();
+
+  // Auth Related Code
+  await setAuthToken(request);
 
   if (formData.get("_method") === "delete") {
     await supabase
@@ -17,6 +24,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 };
 
 export const loader: LoaderFunction = async ({ params }) => {
+  // No need to add auth here, because GET /words is public
   const { data } = await supabase
     .from<Word>("words")
     .select("*")
@@ -28,6 +36,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function Word() {
   const word = useLoaderData<Word>();
+  const supabase = useSupabase();
   let transition = useTransition();
 
   return (
@@ -44,13 +53,24 @@ export default function Word() {
       {word.sentences.map((sentence, i) => (
         <p key={i}>{sentence}</p>
       ))}
-      <Form method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit">Delete</button>
-      </Form>
-      <Form method="get" action={`/words/edit/${word.id}`}>
-        <button type="submit">Edit</button>
-      </Form>
+
+      {/* Adding conditional rendering might cause a warning,
+      We'll deal with it later */}
+      {supabase.auth.user() && (
+        <>
+          <Form method="post">
+            <Input type="hidden" name="_method" value="delete" />
+            <Button type="submit" className="w-full">
+              Delete
+            </Button>
+          </Form>
+          <Form method="get" action={`/words/edit/${word.id}`} className="flex">
+            <Button type="submit" color="primary" className="w-full">
+              Edit
+            </Button>
+          </Form>
+        </>
+      )}
     </div>
   );
 }
